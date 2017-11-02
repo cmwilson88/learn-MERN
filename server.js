3cc84815c094b503f0b48705b9801153c38105c4
Christopher Wilson <cmw.wilson88@gmail.com>
2017-11-01 22:56:30 -0400

    Add boilerplate code and server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const bodyParser = require('body-parser'),
+      mongoose   = require('mongoose'),
+      express    = require('express'),
+      router     = express.Router(),
+      app        = express();
+
+
+const port = process.env.API_PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+   //and remove cacheing so we get the most recent comments
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'API initialized'})
+})
+
+app.use('/api', router)
+
+app.listen(port, function() {
+    console.log(`api running on port ${port}`)
+})
