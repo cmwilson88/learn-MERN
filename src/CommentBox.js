04924cf33bc55d2e1be59c8f7976dee43a740573
Christopher Wilson <cmw.wilson88@gmail.com>
2017-11-05 11:33:13 -0500

    Add front end update/delete methods and input

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -11,7 +11,9 @@ class CommentBox extends Component {
         this.state = { data: [] };
 
         this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
-        this.handleCommentSubmit = this.handleCommentSubmit.bind(this)
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+        this.handleCommentDelete = this.handleCommentDelete.bind(this);
+        this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
     }
 
     loadCommentsFromServer() {
@@ -34,6 +36,19 @@ class CommentBox extends Component {
             })
     }
 
+    handleCommentDelete(id) {
+        axios.delete(`${this.props.url}/${id}`)
+            .then(res => {
+                console.log('Comment deleted')
+            })
+            .catch(err => console.error(err))
+    }
+
+    handleCommentUpdate(id, comment) {
+        axios.put(`${this.props.url}/${id}`, comment)
+            .catch(err => console.error(err));
+    }
+
     componentDidMount() {
         this.loadCommentsFromServer();
         setInterval(this.loadCommentsFromServer, this.props.pollInterval)
@@ -42,7 +57,10 @@ class CommentBox extends Component {
         return (
             <div style={ style.commentBox }>
                 <h2>Comments:</h2>
-                <CommentList data={ this.state.data } />
+                <CommentList
+                    onCommentDelete={this.handleCommentDelete}
+                    onCommentUpdate={this.handleCommentUpdate} 
+                    data={ this.state.data } />
                 <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         )
